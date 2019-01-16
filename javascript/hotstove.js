e585f4a2bf1d03fc7c5154b8f7ca7be3e0737987
Eric Northrop <eric.northrop@gmail.com>
2019-01-15 21:01:39 -0600

    additional notes

--- javascript/hotstove.js
+++ javascript/hotstove.js
@@ -1,6 +1,8 @@
 // Giphy API key: hjdE9lx9cGidHy5CN2GtAf1oyDZ9nEJN
 // remember to query via https
 
+// personal use: reference movie generator here: file:///C:/Users/eric/Google%20Drive/Northwestern/Trilogy/Session%201-8/09-ClickJSON/Unsolved/click-json.html
+
 var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=hjdE9lx9cGidHy5CN2GtAf1oyDZ9nEJN";
 
 $.ajax({

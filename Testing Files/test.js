157db8e604f6d1327f9a09d8cd0820636f7221d0
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-01-16 20:18:33 -0600

    big commit, got buttons added from array, modified HTML

--- Testing Files/test.js
+++ Testing Files/test.js
@@ -14,3 +14,7 @@
 
 var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=dumpster+fire&api_key=hjdE9lx9cGidHy5CN2GtAf1oyDZ9nEJN&limit=5");
 xhr.done(function(data) { console.log("success got data", data); });
+
+function printGif(data) {
+    println(data.data[0].images.original.url);
+}
--- javascript/hotstove.js
+++ javascript/hotstove.js
@@ -1,12 +1,80 @@
+/*
+
+Start of File!
+
 // Giphy API key: hjdE9lx9cGidHy5CN2GtAf1oyDZ9nEJN
 // remember to query via https
-
 // personal use: reference movie generator here: file:///C:/Users/eric/Google%20Drive/Northwestern/Trilogy/Session%201-8/09-ClickJSON/Unsolved/click-json.html
 // Ended up referencing this file a lot.
 
+*/
+
+//  ************** START ATTEMPT AT REAL CODE **************
+
+// Array of initial buttons, named topics per instructions
+var topics = ["salty", "Deal with it", "Dumpster Fire"];
 
 
-var buttonsInit = ["salty", "Deal with it", "Dumpster Fire"]
+
+$(document).ready(function () {
+    // Test log
+    console.log("it works!");
+    // Just get and console log out giphy return(code from Giphy API documentation, modified for assignment)
+    // limit to 10 results per homework instructions
+    var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=dumpster+fire&api_key=hjdE9lx9cGidHy5CN2GtAf1oyDZ9nEJN&limit=10");
+    xhr.done(function (data) { console.log("success got data", data); });
+    // $("#results").html("<img src =" + xhr.data[0].images.original.url + ">");
+    renderButtons();
+
+
+});
+
+// function definitely lifted from the json click assignment
+function renderButtons() {
+
+    // Deleting the buttons prior to adding new movies
+    // (this is necessary otherwise you will have repeat buttons)
+    $("#premade-buttons").empty();
+    console.log("renderbuttons fired!")
+    // Looping through the array of movies
+    for (var i = 0; i < topics.length; i++) {
+        console.log("loop has worked " + i);
+      // Then dynamically generating buttons for each movie in the array
+      // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+      var a = $("<button>");
+      // Adding a class of movie to our button
+      a.addClass("term");
+      // Adding a data-attribute
+      a.attr("data-name", topics[i]);
+      // Providing the initial button text
+      a.text(topics[i]);
+      // Adding the button to the buttons-view div
+      $("#premade-buttons").append(a);
+    }
+  }
+
+
+
+
+
+
+// ************** END ATTEMPT AT REAL CODE **************
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+SHIT CODE BELOW THAT DIDNT WORK
+
+
 // maybe use this below to debug and just pull trending
 // var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=hjdE9lx9cGidHy5CN2GtAf1oyDZ9nEJN";
 
@@ -41,6 +109,6 @@ for (var i = 0; i < buttonsInit.length; i++) {
   $("#buttons-view").append(a);
 }
 }
-
+*/
 
 

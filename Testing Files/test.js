c8c749cfce37012b407e57a81a027161c4fe44a5
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-01-17 00:09:32 -0600

    Final commit of the night, figuring out API call

--- Testing Files/test.js
+++ Testing Files/test.js
@@ -1,20 +1,20 @@
-// function getGif(input) {
+function getGif() {
 
-//     // var gifName = input;
-//     var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=hjdE9lx9cGidHy5CN2GtAf1oyDZ9nEJN&limit=5&search?q=" + input;
+    // var gifName = input;
+    var queryURL = "https://api.giphy.com/v1/gifs/api_key=hjdE9lx9cGidHy5CN2GtAf1oyDZ9nEJN&limit=5&search?q=fire";
   
-//     $.ajax({
-//       url: queryURL,
-//       method: "GET"
-//     }).then(function (response) {
-//       console.log(response);
-//     });
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      console.log(response);
+    });
   
-//   }+
+  }
 
-var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=dumpster+fire&api_key=hjdE9lx9cGidHy5CN2GtAf1oyDZ9nEJN&limit=5");
-xhr.done(function(data) { console.log("success got data", data); });
+// var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=dumpster+fire&api_key=hjdE9lx9cGidHy5CN2GtAf1oyDZ9nEJN&limit=5");
+// xhr.done(function(data) { console.log("success got data", data); });
 
-function printGif(data) {
-    println(data.data[0].images.original.url);
-}
+// function printGif(data) {
+//     println(data.data[0].images.original.url);
+// }
--- javascript/hotstove.js
+++ javascript/hotstove.js
@@ -13,16 +13,22 @@ Start of File!
 
 // Array of initial buttons, named topics per instructions
 var topics = ["Salty", "Deal with it", "Dumpster Fire"];
-
+var userVal = '';
+var gifWad;
 
 
 $(document).ready(function () {
     // Test log
     console.log("it works!");
-    // Just get and console log out giphy return(code from Giphy API documentation, modified for assignment)
+        // Just get and console log out giphy return(code from Giphy API documentation, modified for assignment)
     // limit to 10 results per homework instructions
+    // changed data to response to reduce confusion
     var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=dumpster+fire&api_key=hjdE9lx9cGidHy5CN2GtAf1oyDZ9nEJN&limit=10");
-    xhr.done(function (data) { console.log("success got data", data); });
+    xhr.done(function (response) { 
+        console.log("success got data", response);
+        var gifWad = response.data;
+});
+ 
     // $("#results").html("<img src =" + xhr.data[0].images.original.url + ">");
     renderButtons();
 
@@ -41,15 +47,21 @@ $(document).ready(function () {
 
     // primary gif click functionality
     // not really working and inspiring a lot of rage
-    $(document).on('click', 'premade-buttons.term', function () {
-        alert("Button clicked!");
+    // EDIT 11:06 1/16: This to the rescue!
+    $('#premade-buttons').on('click', '.term', function () {
+        //button clicks and logs
         console.log("button clicked!");
-
+        // somehow try and get the attribute of the button that is clicked and store as butVal
+        var userVal = $(this).attr("data-name");
+        // haha, butVal
+        console.log(userVal);
+        // Stuck here for the night
+        console.log(gifWad[0].id);
     });
+    
+
 
 });
-
-
 
 
 
@@ -87,7 +99,7 @@ function renderButtons() {
 
 
 /*
-SHIT CODE BELOW THAT DIDNT WORK OR MAYBE DID AND I USED IT IN A DIFFERENT WAY
+CODE BELOW THAT DIDNT WORK OR MAYBE DID AND I USED IT IN A DIFFERENT WAY
 
 
 // maybe use this below to debug and just pull trending
